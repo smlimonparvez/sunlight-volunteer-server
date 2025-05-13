7324a47763028d474a3e3086ed0d9f8b46bd9209
smlimonparvez <smlimonparvez@gmail.com>
2025-05-13 17:11:38 +0600

    get all post post details and limited post api created

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w3tuc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,7 +36,8 @@ async function run() {
 
     const postCollection = client.db("volunteer").collection("add_post");
 
-    app.post("/post", async (req, res) => {
+    // add post
+    app.post("/add-post", async (req, res) => {
       const newPost = req.body;
       if (
         !newPost ||
@@ -45,7 +50,29 @@ async function run() {
       const result = await postCollection.insertOne(newPost);
       res.json(result);
     });
-    
+
+    // get all post
+    app.get("/posts", async (req, res) => {
+      const cursor = postCollection.find({});
+      const posts = await cursor.toArray();
+      res.json(posts);
+    })
+
+    // get limited post by ascending order
+    app.get("/limited-posts", async (req, res) => {
+      const cursor = postCollection.find({}).sort({deadline: 1}).limit(6);
+      const limitedPost = await cursor.toArray();
+      res.json(limitedPost);
+    })
+
+    // get post details
+    app.get("/post-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const post = await postCollection.findOne(query);
+      res.json(post);
+    })
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
